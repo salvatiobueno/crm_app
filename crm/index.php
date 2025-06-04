722363a9821a1c456bf25ea904806c852fd39e85
salvador.cuevas <salvador.cuevas@clinicasdiegodeleon.com>
2025-06-04 16:52:37 +0200

    se incluye la función de logout

--- crm/index.php
+++ crm/index.php
@@ -25,6 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <title>Login - CRM TBP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" href="favicon_io/favicon.ico" type="image/x-icon">
 </head>
 <body class="bg-light d-flex align-items-center" style="height:100vh;">
 <div class="container">
