57d03eab7cb1d54759f2d52efe3039310aa5c439
salvador.cuevas <salvador.cuevas@clinicasdiegodeleon.com>
2025-06-04 16:53:26 +0200

    se incluye la función de logout

--- crm/includes/header.php
+++ crm/includes/header.php
@@ -11,6 +11,7 @@ if (!isset($_SESSION['usuario'])) {
     <meta charset="UTF-8">
     <title>Panel - CRM TBP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" href="favicon_io/favicon.ico" type="image/x-icon">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
