09d95531dfbc02f62a0d5982c4243eef369466c5
SalvaC <gatag_@hotmail.com>
2025-06-23 00:59:14 +0200

    Delete botoncompra.php

--- botoncompra.php
+++
@@ -1,9 +0,0 @@
-<script async
-  src="https://js.stripe.com/v3/buy-button.js">
-</script>
-
-<stripe-buy-button
-  buy-button-id="buy_btn_1RRuZlBBsDHVlMxaJxMCp9NC"
-  publishable-key="pk_tesGvlEgNHLiqO2hMbLrzST5LAWsWVkU27GuCQ4AackskxEnfI3Gvyuu4iTam00sWFyTIz4"
->
-</stripe-buy-button>
