75ad7be1fbc05ceff508bb9e3981e9c8e8b96731
SalvaC <gatag_@hotmail.com>
2025-06-23 00:57:01 +0200

    Update botoncompra.php

--- botoncompra.php
+++ botoncompra.php
@@ -4,6 +4,6 @@
 
 <stripe-buy-button
   buy-button-id="buy_btn_1RRuZlBBsDHVlMxaJxMCp9NC"
-  publishable-key="pk_test_51RRZiSBBsDHVlMxaGaiJGxAThPGjFaGvlEgNHLiqO2hMbLrzST5LAWsWVkU27GuCQ4AackskxEnfI3Gvyuu4iTam00sWFyTIz4"
+  publishable-key="pk_tesGvlEgNHLiqO2hMbLrzST5LAWsWVkU27GuCQ4AackskxEnfI3Gvyuu4iTam00sWFyTIz4"
 >
 </stripe-buy-button>
